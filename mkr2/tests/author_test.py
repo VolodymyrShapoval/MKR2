63b79234c6dd56ba1055e60fdd2b75aad278a435
VolodymyrS <volodymyr.sh.05@gmail.com>
2024-05-31 11:45:48 +0300

    added tests

---
+++ mkr2/tests/author_test.py
@@ -0,0 +1,18 @@
+class AuthorViewTests(TestCase):
+    def setUp(self):
+        self.author1 = Author.objects.create(name="Author One")
+        self.author2 = Author.objects.create(name="Author Two")
+
+    def test_author_list_view(self):
+        response = self.client.get(reverse('author_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'author_list.html')
+        self.assertContains(response, self.author1.name)
+        self.assertContains(response, self.author2.name)
+
+    def test_author_detail_view(self):
+        response = self.client.get(reverse('author_detail', args=[self.author1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'author_detail.html')
+        self.assertContains(response, self.author1.name)
+        self.assertNotContains(response, self.author2.name)
---
+++ mkr2/tests/book_test.py
@@ -0,0 +1,18 @@
+class BookViewTests(TestCase):
+    def setUp(self):
+        self.book1 = Book.objects.create(title="Book One", author="Author One", published_date="2023-01-01")
+        self.book2 = Book.objects.create(title="Book Two", author="Author Two", published_date="2023-01-02")
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book_list.html')
+        self.assertContains(response, self.book1.title)
+        self.assertContains(response, self.book2.title)
+
+    def test_book_detail_view(self):
+        response = self.client.get(reverse('book_detail', args=[self.book1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book_detail.html')
+        self.assertContains(response, self.book1.title)
+        self.assertNotContains(response, self.book2.title)
